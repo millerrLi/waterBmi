6e12fd4fa14401a9b327f28d329cee4828ace854
millerr <millerr@water.gov.taipei>
2022-07-14 08:16:47 +0800

    linear layout

--- app/src/main/java/gov/water/bmi/MainActivity.kt
+++ app/src/main/java/gov/water/bmi/MainActivity.kt
@@ -2,10 +2,25 @@ package gov.water.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import gov.water.bmi.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    fun bmi(view:View){
+        val weight = ed_weight.text.toString().toFloat()
+        val height = ed_height.text.toString().toFloat()
+        val bmi = weight / (height * height)
+        Log.d("BMI", bmi.toString())
     }
 }
